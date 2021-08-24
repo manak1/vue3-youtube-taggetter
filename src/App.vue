ec1a4bf3b2da6212574b9d78cb444c6357ad9b4a
manak1 <mana23happy@hotmail.co.jp>
2021-08-25 01:30:34 +0900

    chore: プロジェクトの作成

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container mx-auto text-center">
+    <img alt="Vue logo" class="mx-auto" src="./assets/logo.png" />
+    <h2 class="text-red-300">test</h2>
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import HelloWorld from './components/HelloWorld.vue'
+
+  export default defineComponent({
+    name: 'App',
+    components: {
+      HelloWorld,
+    },
+  })
+</script>
