3bc83ced531ef1c78460c3da7d7fc64630307dba
manak1 <mana23happy@hotmail.co.jp>
2021-09-03 21:45:02 +0900

    feat: InpuTextの作成

---
+++ src/components/input/InputText.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="w-full">
+    <label :for="name" class="text-sm font-bold text-gray-600">
+      {{ label }}
+    </label>
+    <input
+      :modelValue="modelValue"
+      :name="name"
+      :placeholder="placeholder"
+      class="rounded border w-full py-2 px-2 mt-1"
+      @input="onInput($event.target.value)"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, PropType } from 'vue'
+
+  export default defineComponent({
+    name: 'InputText',
+    props: {
+      modelValue: {
+        type: String as PropType<string>,
+        required: true,
+      },
+      name: {
+        type: String as PropType<string>,
+        required: true,
+      },
+      label: {
+        type: String as PropType<string>,
+        default: '',
+      },
+      placeholder: {
+        type: String as PropType<string>,
+        required: true,
+      },
+    },
+    emits: ['update:modelValue'],
+    setup(_, { emit }) {
+      const onInput = (value: string) => {
+        emit('update:modelValue', value)
+      }
+      return {
+        onInput,
+      }
+    },
+  })
+</script>
