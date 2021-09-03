bf74c3c0418965ba7dde3132c963697252856802
manak1 <mana23happy@hotmail.co.jp>
2021-09-03 21:44:28 +0900

    feat: Tagの作成

---
+++ src/components/common/Tag.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    class="
+      flex
+      items-center
+      rounded-full
+      py-1
+      px-2
+      bg-gray-200
+      inline-block
+      cursor-pointer
+    "
+  >
+    <p class="text-gray-700">{{ title }}</p>
+    <a href="#" class="ml-1" @click.prevent="onDelete(title)">
+      <img class="w-4 h-4" src="@/assets/images/close.svg" alt="削除" />
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, PropType } from 'vue'
+  export default defineComponent({
+    props: {
+      title: {
+        type: String as PropType<string>,
+        required: true,
+      },
+      index: {
+        type: Number as PropType<number>,
+        required: true,
+      },
+    },
+    emits: ['onDelete'],
+    setup(_, { emit }) {
+      const onDelete = (title: string) => {
+        console.log(title)
+        emit('onDelete', title)
+      }
+      return {
+        onDelete,
+      }
+    },
+  })
+</script>
+
+<style scoped></style>
