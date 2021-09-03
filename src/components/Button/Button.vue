2cdc5b139f9f44ec299c9a189f0837ce1e97190d
manak1 <mana23happy@hotmail.co.jp>
2021-09-03 21:44:14 +0900

    feat: Buttonの作成

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,38 @@
+<template>
+  <button
+    class="inline-block py-3 w-full hover:opacity-75"
+    :class="disabledClass"
+    :disabled="disabled"
+  >
+    <p class="font-bold text-white">
+      {{ label }}
+    </p>
+  </button>
+</template>
+
+<script lang="ts">
+  import { defineComponent, PropType, computed } from 'vue'
+
+  export default defineComponent({
+    props: {
+      label: {
+        type: String as PropType<string>,
+        required: true,
+      },
+      disabled: {
+        type: Boolean as PropType<boolean>,
+        default: false,
+      },
+    },
+    setup(props) {
+      const disabledClass = computed(() =>
+        props.disabled ? 'pointer-none bg-gray-300' : 'bg-purple-500'
+      )
+      return {
+        disabledClass,
+      }
+    },
+  })
+</script>
+
+<style scoped></style>
