b9d38ad5e64413497d78d71a57b97bd096739dd6
manak1 <mana23happy@hotmail.co.jp>
2021-09-03 21:47:56 +0900

    feat: メインページの作成

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,82 @@
 <template>
-  <div class="container mx-auto text-center">
-    <img alt="Vue logo" class="mx-auto" src="./assets/logo.png" />
-    <h2 class="text-red-300">test</h2>
-    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  </div>
+  <HeaderFooterView>
+    <h1 class="text-center">Youtube Tagゲッター(｀・ω・´)</h1>
+    <form class="mt-5">
+      <InputText
+        name="url"
+        label="URL"
+        placeholder="YouTubeのURLを入力してください。"
+      />
+    </form>
+    <div class="mt-4">
+      <TagList label="タグ一覧" :tags="tags" @onDelete="onDeleteTag" />
+    </div>
+    <div class="mt-12">
+      <Button label="コピーする" @click="copyText" />
+    </div>
+  </HeaderFooterView>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue'
-  import HelloWorld from './components/HelloWorld.vue'
+  const tagList = new Array(3).fill(null).map((_, index) => {
+    return {
+      title: `テスト${index}`,
+      index,
+    }
+  })
+
+  import { defineComponent, reactive, ref, onMounted } from 'vue'
+
+  import InputText from '@/components/input/InputText.vue'
+  import TagList from '@/components/common/TagList.vue'
+  import HeaderFooterView from '@/layout/HeaderFooterView.vue'
+  import Button from '@/components/button/Button.vue'
+
+  import { clipboardCopyText } from '@/utils/clipboard.ts'
+
+  import { Tag } from '../src/types/index'
 
   export default defineComponent({
     name: 'App',
     components: {
-      HelloWorld,
+      HeaderFooterView,
+      InputText,
+      TagList,
+      Button,
+    },
+    setup() {
+      const tags = ref<Tag[]>([])
+      const form = reactive({
+        url: '',
+      })
+
+      onMounted(() => {
+        tags.value = tagList
+      })
+
+      const copyText = async () => {
+        await clipboardCopyText('テストだよ').catch((error: Error) => {
+          throw error
+        })
+      }
+
+      const onDeleteTag = (title: string) => {
+        tags.value = tags.value.filter((tag) => {
+          return tag.title !== title
+        })
+      }
+
+      return {
+        form,
+        copyText,
+        onDeleteTag,
+        tags,
+      }
+    },
+    data() {
+      return {
+        tagList,
+      }
     },
   })
 </script>
