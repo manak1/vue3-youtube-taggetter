ec0ff8c76035381d7f33b407401f24645d9a61e1
manak1 <mana23happy@hotmail.co.jp>
2021-09-03 21:43:43 +0900

    feat: Headerの作成

---
+++ src/components/common/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="w-full p-3 text-left">
+    <h1>Youtube Tagゲッター(｀・ω・´)</h1>
+  </header>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'Header',
+  })
+</script>
