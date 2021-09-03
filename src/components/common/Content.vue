45f4a3b6a8864ebb07c11b089a8df016029f1d8f
manak1 <mana23happy@hotmail.co.jp>
2021-09-03 21:45:18 +0900

    feat: Contentの作成

---
+++ src/components/common/Content.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-3/5 px-6 pt-6 pb-10 mx-auto rounded-md bg-white shadow content">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({})
+</script>
+
+<style scoped>
+  .content {
+    max-width: 640px;
+  }
+</style>
