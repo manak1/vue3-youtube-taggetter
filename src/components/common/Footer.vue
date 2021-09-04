053149047d2ff37a15612effeb99f344537b4ac6
manak1 <mana23happy@hotmail.co.jp>
2021-09-05 03:46:39 +0900

    feat: footerにツイッターのリンクを追加

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,6 +1,18 @@
 <template>
   <footer class="text-center py-3">
-    <small>&copy; copyright hungrypenguin</small>
+    <div class="inline-block">
+      <a
+        href="https://twitter.com/mikeanakida"
+        class="flex items-center justify-center"
+      >
+        <p class="text-sm">ついったー( ・∀・)σ</p>
+        <img
+          src="@/assets/images/mikeanakida.jpeg"
+          class="ml-2 w-8 h-8 rounded-full border-2 shadow-lg"
+          alt="作者"
+        />
+      </a>
+    </div>
   </footer>
 </template>
 
