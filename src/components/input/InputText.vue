615bce27125bdaf3a76c13d9ec6f11e2f7483ce5
manak1 <mana23happy@hotmail.co.jp>
2021-09-05 03:52:00 +0900

    feat: テキストをリセットできるように

--- src/components/input/InputText.vue
+++ src/components/input/InputText.vue
@@ -3,18 +3,39 @@
     <label :for="name" class="text-sm font-bold text-gray-600">
       {{ label }}
     </label>
-    <input
-      :modelValue="modelValue"
-      :name="name"
-      :placeholder="placeholder"
-      class="rounded border w-full py-2 px-2 mt-1"
-      @input="onInput($event.target.value)"
-    />
+    <div class="relative">
+      <input
+        ref="inputRef"
+        :modelValue="modelValue"
+        :name="name"
+        :placeholder="placeholder"
+        class="text-sm sm:text-base rounded border w-full py-2 px-2 mt-1"
+        autocomplete="off"
+        @input="onInput($event.target.value)"
+      />
+      <img
+        class="
+          absolute
+          w-3
+          h-3
+          sm:w-4 sm:h-4
+          right-2
+          sm:right-3
+          top-4
+          opacity-50
+          cursor-pointer
+        "
+        src="@/assets/images/times.svg"
+        alt="削除"
+        tabindex="0"
+        @click.prevent="onClear"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent, PropType } from 'vue'
+  import { defineComponent, PropType, ref } from 'vue'
 
   export default defineComponent({
     name: 'InputText',
@@ -36,13 +57,21 @@
         required: true,
       },
     },
-    emits: ['update:modelValue'],
+    emits: ['update:modelValue', 'clear'],
     setup(_, { emit }) {
+      const inputRef = ref<HTMLInputElement>()
       const onInput = (value: string) => {
         emit('update:modelValue', value)
       }
+      const onClear = () => {
+        if (!inputRef.value) return
+        emit('update:modelValue', '')
+        inputRef.value.value = ''
+      }
       return {
+        inputRef,
         onInput,
+        onClear,
       }
     },
   })
