43b3886131ca7b54a6a3deb513bfab0bac1346e8
manak1 <mana23happy@hotmail.co.jp>
2021-09-07 11:08:26 +0900

    feat: API連携

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,46 @@
 <template>
   <HeaderFooterView>
     <h1 class="text-center">Youtube Tagゲッター(｀・ω・´)</h1>
-    <form class="mt-5">
+    <form class="mt-5 space-y-4">
       <InputText
+        v-model="form.url"
         name="url"
         label="URL"
-        placeholder="YouTubeのURLを入力してください。"
+        placeholder="YouTubeのURLを入力してね( ・∇・)"
+      />
+      <Button
+        label="タグを検索する"
+        :disabled="inProgress || isSameUrl"
+        @click.prevent="searchTags"
       />
     </form>
     <div class="mt-4">
       <TagList label="タグ一覧" :tags="tags" @onDelete="onDeleteTag" />
     </div>
-    <div class="mt-12">
-      <Button label="コピーする" @click="copyText" />
+    <div class="mt-4">
+      <Button
+        :disabled="isTagsEmpty"
+        label="コピーする"
+        @click.prevent="copyText"
+      />
     </div>
   </HeaderFooterView>
 </template>
 
 <script lang="ts">
-  const tagList = new Array(3).fill(null).map((_, index) => {
-    return {
-      title: `テスト${index}`,
-      index,
-    }
-  })
-
-  import { defineComponent, reactive, ref, onMounted } from 'vue'
+  import { defineComponent, reactive, ref, computed } from 'vue'
 
   import InputText from '@/components/input/InputText.vue'
   import TagList from '@/components/common/TagList.vue'
-  import HeaderFooterView from '@/layout/HeaderFooterView.vue'
   import Button from '@/components/button/Button.vue'
+  import HeaderFooterView from '@/layout/HeaderFooterView.vue'
 
-  import { clipboardCopyText } from '@/utils/clipboard.ts'
+  import { clipboardCopyText } from './utils/clipboard'
+  import { getYoutubeId } from './utils/youtube'
+  import { sortByLength } from './utils/sort'
 
-  import { Tag } from '../src/types/index'
+  import { useGetYoutubeTag } from './hooks/api/youtube'
+  import { isNullOrUndefined } from './utils/isNullOrUndefined'
 
   export default defineComponent({
     name: 'App',
@@ -45,37 +51,63 @@
       Button,
     },
     setup() {
-      const tags = ref<Tag[]>([])
+      const { fetch, inProgress } = useGetYoutubeTag()
+
+      const lastFetchUrl = ref('')
+      const tags = ref<string[] | null>(null)
       const form = reactive({
         url: '',
       })
 
-      onMounted(() => {
-        tags.value = tagList
-      })
-
       const copyText = async () => {
-        await clipboardCopyText('テストだよ').catch((error: Error) => {
+        if (!tags.value) return
+        const copyText = tags.value.join(',')
+        await clipboardCopyText(copyText).catch((error: Error) => {
           throw error
         })
+        alert('タグがコピーされたよ！( ・∇・)')
+      }
+
+      const searchTags = async () => {
+        const videoId = getYoutubeId(form.url)
+        if (!videoId) {
+          alert('YouTubeのURLを入力してね(´・ω・`)')
+          return
+        }
+
+        const tagList = await fetch(videoId).catch((error) => {
+          throw error
+        })
+        lastFetchUrl.value = form.url
+        tags.value = sortByLength(tagList as string[])
       }
 
       const onDeleteTag = (title: string) => {
+        if (!tags.value) return
         tags.value = tags.value.filter((tag) => {
-          return tag.title !== title
+          return tag !== title
         })
       }
+
+      const isTagsEmpty = computed(() => {
+        return isNullOrUndefined(tags.value)
+          ? true
+          : !!(tags.value && tags.value?.length === 0)
+      })
+
+      const isSameUrl = computed(() => {
+        return form.url === lastFetchUrl.value
+      })
 
       return {
         form,
         copyText,
         onDeleteTag,
         tags,
-      }
-    },
-    data() {
-      return {
-        tagList,
+        isTagsEmpty,
+        searchTags,
+        inProgress,
+        isSameUrl,
       }
     },
   })
